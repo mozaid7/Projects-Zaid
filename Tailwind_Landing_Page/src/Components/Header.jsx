e6364716e2526b04f675486aca93e54533972874
Mohammed Zaid <mohammedzee98@gmail.com>
2024-09-13 16:59:00 +0530

    Header part completed & images added to assests

--- Tailwind_Landing_Page/src/Components/Header.jsx
+++ Tailwind_Landing_Page/src/Components/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FaBars } from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='flex justify-between'>
+      <div className='flex items-center justify-center gap-2'>
+        <img src="./public/assets/logo.svg" alt="logo"></img>
+        <button className='bg-gradient-to-r from-orange-400 to-red-400 text-xs text-white px-2.5 py-1.5 rounded-2xl'>Hoster is Hiring</button>
+      </div>
+      <div>
+        <FaBars/>
+      </div>
     </div>
   )
 }
