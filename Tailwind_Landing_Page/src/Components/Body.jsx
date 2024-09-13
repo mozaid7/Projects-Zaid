e8de8f639317532ff0810656dd110aa10a350a7a
Mohammed Zaid <mohammedzee98@gmail.com>
2024-09-13 18:18:10 +0530

    made responsive for lg

--- Tailwind_Landing_Page/src/Components/Body.jsx
+++ Tailwind_Landing_Page/src/Components/Body.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Body = () => {
   return (
-    <div className="space-y-4">
-      <div className="flex items-center justify-center">
-        <img src="./assets/Blue-Shape.svg" alt="1st" className="-rotate-[45deg] h-64 md:h-72"/>
-        <img src="./assets/Pink-Shape.svg" alt="2nd" className="absolute -rotate-[30deg] h-64 md:h-72"/>
-        <img src="./assets/Purple-Shape.svg" alt="3rd" className="absolute -rotate-[15deg] h-64 md:h-72"/>
-        <img src="./assets/Hero-Model.png" alt="4th" className="absolute h-64 md:h-72"/>
+    <div className="space-y-4 lg:flex">
+      <div className="flex items-center justify-center lg:flex-1 lg:order-2 lg:justify-end">
+        <img src="./assets/Blue-Shape.svg" alt="1st" className="-rotate-[45deg] h-64 md:h-72 lg:h-[400px]"/>
+        <img src="./assets/Pink-Shape.svg" alt="2nd" className="absolute -rotate-[30deg] h-64 md:h-72 lg:h-[400px]"/>
+        <img src="./assets/Purple-Shape.svg" alt="3rd" className="absolute -rotate-[15deg] h-64 md:h-72lg:h-[400px]"/>
+        <img src="./assets/Hero-Model.png" alt="4th" className="absolute h-64 md:h-72 lg:h-[400px]"/>
       </div>
-      <div>
+      <div className="lg:flex-1 lg:order-1">
         <h1 className="text-5xl font-bold font-playfair leading-tight">Host your website in less than 5 minutes</h1>
         <p className="font-lato text-gray-400">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, blanditiis culpa quos hic ex dolores maxime vel commodi laudantium consectetur.</p>
         <form action="" className="flex flex-col gap-4 md:flex-row">
--- Tailwind_Landing_Page/src/Components/Header.jsx
+++ Tailwind_Landing_Page/src/Components/Header.jsx
@@ -8,7 +8,16 @@ const Header = () => {
         <img src="./public/assets/logo.svg" alt="logo"></img>
         <button className='bg-gradient-to-r from-orange-400 to-red-400 text-xs text-white px-2.5 py-1.5 rounded-2xl'>Hoster is Hiring</button>
       </div>
-      <div>
+      <ul className='hidden lg:flex justify-between items-center font-lato text-gray-400 gap-6'>
+        <li><a href="#">Plans</a></li>
+        <li><a href="#">Find Domain</a></li>
+        <li><a href="#">Why Hoster</a></li>
+      </ul>
+      <div className='hidden lg:flex justify-center items-center font-lato gap-6'>
+        <a href="#" className='text-gray-400'>Sign In</a>
+        <button className="rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white">Join Waitlist</button>
+      </div>
+      <div className='lg:hidden'>
         <FaBars/>
       </div>
     </div>
